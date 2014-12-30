5d7e4bea0e2531ea32bce7de74af5b3e7cb89174
Daniel "Mevoroth" Quach <daniel@quach.inifo>
2014-12-30 16:45:20 +0100

    Scripted Actions Queue and Scripted Action

---
+++ ScriptedAction.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptedAction : MonoBehaviour
+{
+	public float m_preWait = 0f;
+	public float PreWait
+	{
+		get { return m_preWait; }
+	}
+	public float m_postWait = 0f;
+	public float PostWait
+	{
+		get { return m_postWait; }
+	}
+	public float m_actionTimer = 1f;
+	public float ActionTimer
+	{
+		get { return m_actionTimer; }
+	}
+	public float m_stepWait = 0.05f;
+	public float StepWait
+	{
+		get { return m_stepWait; }
+	}
+	private float m_completion = 0f;
+	public float Completion
+	{
+		get { return m_completion; }
+	}
+	public bool Finished
+	{
+		get { return m_completion == 1f; }
+	}
+
+	public virtual void Step(float completion)
+	{
+		m_completion = completion;
+	}
+
+	public virtual void OnScriptedActionFinished()
+	{
+	}
+
+	public void OnScriptedActionStart()
+	{
+	}
+}
---
+++ ScriptedActionsQueue.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptedActionsQueue : MonoBehaviour
+{
+	public Queue m_actions = new Queue();
+
+	IEnumerator ExecuteQueue()
+	{
+		for (;;)
+		{
+			while (m_actions.Count > 0)
+			{
+				ScriptedAction action = (ScriptedAction)m_actions.Dequeue();
+				yield return new WaitForSeconds(action.PreWait);
+				action.OnScriptedActionStart();
+				for (float i = 0f, timer = action.ActionTimer, step = action.StepWait; i < timer; i += step)
+				{
+					action.Step(i / timer);
+					yield return new WaitForSeconds(step);
+				}
+				action.Step(1f);
+				action.OnScriptedActionFinished();
+				Debug.Log(action.PostWait);
+				yield return new WaitForSeconds(action.PostWait);
+			}
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	public void Awake()
+	{
+		StartCoroutine("ExecuteQueue");
+	}
+
+	public void Push(ScriptedAction action)
+	{
+		m_actions.Enqueue(action);
+	}
+}
